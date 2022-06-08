259a0c784ad22b278796ea0102ee14bc48744697
Anjali korada <97424576+901456@users.noreply.github.com>
2022-06-08 13:26:06 +0530

    #include<stdio.h> int main() {     int x,y,z,k=0,n;     scanf("%d",&n);     x=0;     y=1;     z=x+y;     printf("%d %d ",x,y);     while(z!=0)     {         printf("%d ",z);         x=y;         y=z;         z=x+y;         k++;         if(k==n-2)         {             break;         }     } }

--- fibonacci.c
+++ fibonacci.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
 int main()
 {
-	int n,x=0,y=1,z,i;
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		printf("%d ",x);
-		z=x+y;
-		x=y;
-		y=z;
-	}
-	return 0;
+    int x,y,z,k=0,n;
+    scanf("%d",&n);
+    x=0;
+    y=1;
+    z=x+y;
+    printf("%d %d ",x,y);
+    while(z!=0)
+    {
+        printf("%d ",z);
+        x=y;
+        y=z;
+        z=x+y;
+        k++;
+        if(k==n-2)
+        {
+            break;
+        }
+    }
 }
