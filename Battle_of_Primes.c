f6068ba4d7dd19a49fadc37560fb54ce001c7423
Anjali korada <97424576+901456@users.noreply.github.com>
2022-06-11 12:41:12 +0530

    #include<stdio.h> int isprime(int n) {     int i,p=0,k;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             p++;         }     }     if(p==2)     {         return 1;     }     return 0; } int main() {     int n,m,i,k,p;     scanf("%d%d",&n,&m);     p=n+m;     for(i=1;i<=2*p;i++)     {         k=isprime(p+i);         if(k==1)         {             printf("%d ",i);             break;         }     } }

--- Battle_of_Primes.c
+++ Battle_of_Primes.c
@@ -1,32 +1,31 @@
 #include<stdio.h>
-int isprime (int n)
+int isprime(int n)
 {
-    int i,c=0,k;
+    int i,p=0,k;
     for(i=1;i<=n;i++)
     {
         if(n%i==0)
         {
-            c++;
-            k=i;
+            p++;
         }
     }
-    if(c==2)
+    if(p==2)
     {
-        return k;
+        return 1;
     }
     return 0;
 }
 int main()
 {
-    int n,m,k,i,j,p;
+    int n,m,i,k,p;
     scanf("%d%d",&n,&m);
     p=n+m;
     for(i=1;i<=2*p;i++)
     {
         k=isprime(p+i);
-        if(k!=0)
+        if(k==1)
         {
-            printf("%d",i);
+            printf("%d ",i);
             break;
         }
     }
