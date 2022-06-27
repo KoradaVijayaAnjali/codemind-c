91cae4d1907769f307f3ac5acff5380c79aa979d
Anjali korada <97424576+901456@users.noreply.github.com>
2022-06-28 02:05:40 +0530

    #include<stdio.h> int main() {     int n,sum=0,temp=10;     scanf("%d",&n);     while(temp/10!=0)     {         if(n==0)         {             temp=sum;             n=sum;             sum=0;         }         else         {             sum+=n%10;             n=n/10;         }     }     printf("%d",temp); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,23 +1,21 @@
 #include<stdio.h>
-int add_digit(int n)
-{
-    int d,sum=0;
-    while(n!=0)
-    {
-        d=n%10;
-        n=n/10;
-        sum=sum+d;
-    }
-    if(sum<10)
-    {
-        return sum;
-    }
-    return add_digit(sum);
-}
 int main()
 {
-    int n,k;
+    int n,sum=0,temp=10;
     scanf("%d",&n);
-    k=add_digit(n);
-    printf("%d",k);
+    while(temp/10!=0)
+    {
+        if(n==0)
+        {
+            temp=sum;
+            n=sum;
+            sum=0;
+        }
+        else
+        {
+            sum+=n%10;
+            n=n/10;
+        }
+    }
+    printf("%d",temp);
 }
